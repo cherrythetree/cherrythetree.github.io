fbebbd7982277407e9ed6aed9eca3a39d83209bf
Ryan Derek Sarmiento <78667217+cherrythetree@users.noreply.github.com>
2025-04-11 00:51:56 +0800

    Update script,js

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ VANTA.FOG({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
+  minHeight: 100.00,
   minWidth: 200.00,
   highlightColor: "#ef6565",
   midtoneColor: "#a40000",
